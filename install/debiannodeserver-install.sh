14327c2cffa64b5b236cccdae36d64332a015080
Gareth Cheyne <Gareth.Cheyne@nz.harveynorman.com>
2023-07-25 21:57:14 +1200

    update

--- install/debiannodeserver-install.sh
+++ install/debiannodeserver-install.sh
@@ -19,26 +19,34 @@ $STD apt-get install -y sudo
 $STD apt-get install -y git
 msg_ok "Installed Dependencies"
 
-msg_info "Setting up Node.js Repository"
+msg_info "Setting up Node v18.x Repository"
 curl -sSL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
 bash nodesource_setup.sh
-update_os
-msg_ok "Set up Node.js Repository"
-msg_ok node -v
+$STD apt-get update 
 
-msg_info "Installing Node.js"
+msg_info "Installing Node v18.x"
 $STD apt-get install -y nodejs 
 $STD apt-get install -y make 
 $STD apt-get install -y g++ 
 $STD apt-get install -y gcc 
-msg_ok "Installed Node.js"
 
 msg_info "Installing Yarn"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list &>/dev/null
 $STD apt-get update 
 $STD apt-get install -y yarn
-msg_ok "Installed Yarn"
+
+clear
+
+while true; do
+    read -p "Do you want to pull a repo?(y/n)?" yn
+    case $yn in
+    [Yy]*) break ;;
+    [Nn]*) exit ;;
+    *) echo "Please answer yes or no." ;;
+    esac
+done
+
 
 motd_ssh
 customize
