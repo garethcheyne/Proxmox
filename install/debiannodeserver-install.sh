754d5a43b6d6ec8eff87243f3bacde491f27aabf
Gareth Cheyne <Gareth.Cheyne@nz.harveynorman.com>
2023-07-25 21:44:16 +1200

    update

--- install/debiannodeserver-install.sh
+++ install/debiannodeserver-install.sh
@@ -20,7 +20,7 @@ $STD apt-get install -y git
 msg_ok "Installed Dependencies"
 
 msg_info "Setting up Node.js Repository"
-curl -sSL https://deb.nodesource.com/setup_18.x curl -o nodesource_setup.sh
+curl -sSL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
 bash nodesource_setup.sh
 update_os
 msg_ok "Set up Node.js Repository"
@@ -33,12 +33,12 @@ $STD apt-get install -y g++
 $STD apt-get install -y gcc 
 msg_ok "Installed Node.js"
 
-# msg_info "Installing Yarn"
-# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - &>/dev/null
-# echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list &>/dev/null
-# apt-get update &>/dev/null
-# apt-get install -y yarn &>/dev/null
-# msg_ok "Installed Yarn"
+msg_info "Installing Yarn"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list &>/dev/null
+$STD apt-get update 
+$STD apt-get install -y yarn
+$STD msg_ok "Installed Yarn"
 
 motd_ssh
 customize
