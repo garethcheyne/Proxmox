6bc3baf14de58efbe0295ed309c769f18c39d6a9
Gareth Cheyne <Gareth.Cheyne@nz.harveynorman.com>
2023-07-25 21:30:06 +1200

    update

--- install/debiannodeserver.sh
+++ install/debiannodeserver.sh
@@ -19,21 +19,19 @@ $STD apt-get install -y sudo
 $STD apt-get install -y git
 msg_ok "Installed Dependencies"
 
+# msg_info "Setting up Node.js Repository"
+# curl -sSL https://deb.nodesource.com/setup_18.x curl -o nodesource_setup.sh
+# bash nodesource_setup.sh
+# update_os
+# msg_ok "Set up Node.js Repository"
+# msg_ok node -v
 
-msg_info "Setting up Node.js Repository"
-curl -sSL https://deb.nodesource.com/setup_18.x curl -o nodesource_setup.sh
-bash nodesource_setup.sh
-update_os
-msg_ok "Set up Node.js Repository"
-msg_ok node -v
-
-msg_info "Installing Node.js"
-$STD apt-get install -y nodejs 
-$STD apt-get install -y make 
-$STD apt-get install -y g++ 
-$STD apt-get install -y gcc 
-msg_ok "Installed Node.js"
-
+# msg_info "Installing Node.js"
+# $STD apt-get install -y nodejs 
+# $STD apt-get install -y make 
+# $STD apt-get install -y g++ 
+# $STD apt-get install -y gcc 
+# msg_ok "Installed Node.js"
 
 # msg_info "Installing Yarn"
 # curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - &>/dev/null
